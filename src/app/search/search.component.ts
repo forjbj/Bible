c86a8e312d2c8512e3bef2a84812de8dfef30a0f
biblepwa <biblepwa@gmail.com>
2022-03-17 13:59:53 +1000

    better still

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -85,7 +85,7 @@ export class SearchComponent implements OnInit, AfterViewInit {
       this.bibleService.searchRequest = req;
       this.bibleService.searchResults = wasm.search( this.checkedNumber, req, this.accuracy)
       this.spinner = false;
-    }, 500); // give it a moment to redraw
+    }, 100); // give it a moment to redraw
     window.scrollTo(0,0); // bring new search to top of page
   }
   @HostListener('window:scroll', []) scrolled() {    
